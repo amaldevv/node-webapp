8954e081f4f53bf1595e74215965a248b2fe3a4d
amaldevv <amaldevv@outlook.com>
2017-10-12 09:18:44 +0530

    Added dockerfile for WEbAPI, Modified Server.js to consume API method

--- webapp/server.js
+++ webapp/server.js
@@ -1,7 +1,7 @@
 'use strict';
-
+var os = require('os');
 const express = require('express');
-
+const request = require('request');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -11,6 +11,19 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
+// api ------------------------------------------------------------
+app.get('/api', function (req, res) {
+  
+
+  
+
+  // Invoke service
+  request('http://localhost:5000', function (error, response, body) {
+      if(error) console.log(error);
+      res.send('Hello from service A running on ' + os.hostname() + ' and ' + body);
+      console.log('Hello from service A running on ' + os.hostname() + ' and ' + body);
+  });
+}); 
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
